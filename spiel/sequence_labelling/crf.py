8268ee0c18f36db3f7b7f862a5a94678d39e7669
Adoxography <gstill@uw.edu>
2018-11-27 13:25:40 -0600

    Make variable names more descriptive

--- spiel/sequence_labelling/crf.py
+++ spiel/sequence_labelling/crf.py
@@ -38,15 +38,15 @@ class SequenceLabeller:
         self.model = model
 
     @staticmethod
-    def build(xs, ys, **kwargs):
+    def build(sequences, labels, **kwargs):
         """
         Builds a sequence classifier from x/y pairs
 
-        :param xs: A list of sequences, with each member of the sequence
+        :param sequences: A list of sequences, with each member of the sequence
                    represented as features
-        :type xs: list of list of dict
-        :param ys: The corresponding labels for each sequence
-        :type ys: list of list of str
+        :type sequences: list of list of dict
+        :param labels: The corresponding labels for each sequence
+        :type labels: list of list of str
         :param kwargs: arguments to override the defaults given to the
                        underlying CRF
         :return: A trained sequence classifier based on the provided training
@@ -65,46 +65,46 @@ class SequenceLabeller:
             params.update(kwargs)
 
         model = CRF(**params)
-        model.fit(xs, ys)
+        model.fit(sequences, labels)
         return SequenceLabeller(model)
 
     @staticmethod
-    def grid_search(xs, ys):
+    def grid_search(sequences, labels):
         """
         Conducts a grid search to find an optimal CRF model, given a set of x/y
         pairs
 
-        :param xs: A list of sequences, with each member of the sequence
+        :param sequences: A list of sequences, with each member of the sequence
                    represented as features
-        :type xs: list of list of dict
-        :param ys: The corresponding labels for each sequence
-        :type ys: list of list of str
+        :type sequences: list of list of dict
+        :param labels: The corresponding labels for each sequence
+        :type labels: list of list of str
         :param kwargs: arguments to override the defaults given to the
                        underlying CRF
         :return: A trained sequence classifier based on the provided training
                  data
         :rtype: SequenceLabeller
         """
-        rs = _grid_search(xs, ys)
-        return SequenceLabeller(rs.best_estimator_)
+        search = _grid_search(sequences, labels)
+        return SequenceLabeller(search.best_estimator_)
 
     def save(self, path):
         """
         Saves the model to the specified path
         """
-        with open(path, 'wb') as f:
-            pickle.dump(self.model, f)
+        with open(path, 'wb') as model_file:
+            pickle.dump(self.model, model_file)
 
     @staticmethod
     def load(path):
         """
         Loads a saved model from a specified path
         """
-        with open(path, 'rb') as f:
-            model = pickle.load(f)
+        with open(path, 'rb') as model_file:
+            model = pickle.load(model_file)
         return SequenceLabeller(model)
 
-    def predict(self, x):
+    def predict(self, sequence):
         """
         Predicts the label sequence of a single sequence
 
@@ -113,44 +113,44 @@ class SequenceLabeller:
         :return: The label sequence predicted for the given sequence
         :rtype: list of str
         """
-        return self.model.predict_single(x)
+        return self.model.predict_single(sequence)
 
-    def predict_many(self, xs):
+    def predict_many(self, sequences):
         """
         Predicts the label sequence of a list of sequences
 
-        :param xs: The sequences to label
-        :type xs: list of list of dict
+        :param sequences: The sequences to label
+        :type sequences: list of list of dict
         :return: The label sequence predicted for each sequence
         :rtype: list of list of str
         """
-        return self.model.predict(xs)
+        return self.model.predict(sequences)
 
-    def evaluate(self, xs, ys):
+    def evaluate(self, sequences, labels):
         """
         Returns the F1 score give a set of sequences and expected labels
 
-        :param xs: A list of sequences, with each member of the sequence
-                   represented as features
-        :type xs: list of list of dict
-        :param ys: The corresponding labels for each sequence
-        :type ys: list of list of str
+        :param sequences: A list of sequences, with each member of the sequence
+                          represented as features
+        :type sequences: list of list of dict
+        :param labels: The corresponding labels for each sequence
+        :type labels: list of list of str
         :return: The F1 score from classifying the sequences
         :rtype: float
         """
-        pred = self.predict_many(xs)
-        return flat_f1_score(ys, pred, average='weighted')
+        pred = self.predict_many(sequences)
+        return flat_f1_score(labels, pred, average='weighted')
 
 
-def _grid_search(x, y):
-    labels = list(set(flatten(y)))
+def _grid_search(sequences, labels):
+    label_set = list(set(flatten(labels)))
     model = CRF(algorithm=DEFAULT_ALGORITHM,
                 max_iterations=DEFAULT_MAX_ITERATIONS,
                 all_possible_transitions=DEFAULT_ALL_POSSIBLE_TRANSITIONS)
     params_space = {'c1': expon(scale=0.5), 'c2': expon(scale=0.05)}
     f1_scorer = make_scorer(flat_f1_score, average='weighted',
-                            labels=labels)
-    rs = RandomizedSearchCV(model, params_space, cv=3, n_jobs=-1, n_iter=50,
-                            scoring=f1_scorer)
-    rs.fit(x, y)
-    return rs
+                            labels=label_set)
+    search = RandomizedSearchCV(model, params_space, cv=3, n_jobs=-1,
+                                n_iter=50, scoring=f1_scorer)
+    search.fit(sequences, labels)
+    return search
