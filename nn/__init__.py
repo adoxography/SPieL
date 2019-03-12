c8bc60618da3707234590350c5282801777e5b92
Adoxography <gstill@uw.edu>
2019-03-12 13:25:24 -0500

    Add Southwestern Ojibwe data and problem definitions

--- nn/__init__.py
+++ nn/__init__.py
@@ -3,6 +3,6 @@ spiel.nn.problem_defs
 
 Module for tensor2tensor problem definitions
 """
-__all__ = ['proto_algonquian']
+__all__ = ['proto_algonquian', 'southwestern_ojibwe']
 
-from . import proto_algonquian
+from . import proto_algonquian, southwestern_ojibwe
